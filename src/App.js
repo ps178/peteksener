03f9041dc1c814c6a10580fb72fc3149898adcdf
ps178 <peteksener9@gmail.com>
2023-01-08 12:41:05 -0500

    chane route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
